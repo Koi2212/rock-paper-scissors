10269b997a043a4ed9d2b7c140ce8c8bb224126a
Koi2212 <funnymine2212@gmail.com>
2023-12-25 20:21:39 +0700

    Fisrt trial

---
+++ gam.js
@@ -0,0 +1,46 @@
+function getComputerChoice() {
+    switch(Math.floor(Math.random() * 3)) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Tie !";
+    } else if (
+        playerSelection == "rock" && computerSelection == "scissors" ||
+        playerSelection == "paper" && computerSelection == "rock" ||
+        playerSelection == "scissors" && computerSelection == "paper"
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+function game() {
+    let playerCore = 0;
+    let computerCore = 0;
+    for (let i = 1; i <= 5; i++) {
+        console.log(`Round ${i}`);
+        let result = playRound(window.prompt("Your choice ?",""), getComputerChoice());
+        if (result.charAt(4) == "w") {
+            playerCore++;
+        } else if (result.charAt(4) == "l") {
+            computerCore++;
+        } 
+        console.log(result);
+    }
+    if (playerCore > computerCore) {
+        console.log("You win the final !");
+    } else if (playerCore < computerCore) {
+        console.log("You lose the final !");
+    } else {
+        console.log("Tie game!")
+    }
+}
